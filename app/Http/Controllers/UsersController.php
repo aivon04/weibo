c2419e8009c29d60eebb7c4c40527295c0a0dcaa
aivon <novia@musicmarketing.ca>
2019-01-28 18:35:19 +0000

    访问策略

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,18 @@ use Auth;
 
 class UsersController extends Controller
 {
+	public function __construct()
+	{
+		$this->middleware('auth',[
+			'except' => ['show','create','store']
+		]);
+		
+		$this->middleware('guest',[
+			'only' => ['create']
+		]);
+	}
+
+
 	public function create()
 	{
 		return view('users.create');
@@ -39,25 +51,27 @@ class UsersController extends Controller
 
 	public function edit(User $user)
 	{
+		$this->authorize('update', $user);
 		return view('users.edit', compact('user'));
 	}
 
-	 public function update(User $user, Request $request)
-    {
-        $this->validate($request, [
-            'name' => 'required|max:50',
-            'password' => 'nullable|confirmed|min:6'
-        ]);
+	public function update(User $user, Request $request)
+	{
+		$this->authorize('update', $user);
+		$this->validate($request, [
+			'name' => 'required|max:50',
+			'password' => 'nullable|confirmed|min:6'
+		]);
 
-        $data = [];
-        $data['name'] = $request->name;
-        if ($request->password) {
-            $data['password'] = bcrypt($request->password);
-        }
-        $user->update($data);
+		$data = [];
+		$data['name'] = $request->name;
+		if ($request->password) {
+			$data['password'] = bcrypt($request->password);
+		}
+		$user->update($data);
 
-        session()->flash('success', '个人资料更新成功！');
+		session()->flash('success', '个人资料更新成功！');
 
-        return redirect()->route('users.show', $user);
-    }
+		return redirect()->route('users.show', $user);
+	}
 }
